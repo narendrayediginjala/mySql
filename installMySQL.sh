35d392ac8cc73000476ff508366ef7eb752cd486
Narendra Reddy Yediginjala <narendra.yediginjala@gmail.com>
2020-03-10 14:44:07 -0700

    Corrected syntax
    Corrected syntax

--- installMySQL.sh
+++ installMySQL.sh
@@ -6,14 +6,16 @@ sudo apt update
 
 aptupdateresult=$?
 
-if [[ $aptupdateresult -ne 0 ]]; then
+if [ $aptupdateresult -ne 0 ]; then
 echo "there is problem updating apt"
+exit 1
 fi
 
 sudo apt install mysql-server
 
 mysqlinstallstatus=$?
 
-if [[ $mysqlinstallstatus -ne 0 ]]; then
+if [ $mysqlinstallstatus -ne 0 ]; then
 echo "there is problem installing mysql"
+exit 1
 fi
